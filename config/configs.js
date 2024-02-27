220a157b063323b8a9e42bee1b6e416a688576e2
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-02-27 14:30:02 +0100

    complete set up

---
+++ config/configs.js
@@ -0,0 +1,7 @@
+// Set the connection string based from the config vars of the production server
+// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
+
+module.exports = {
+  db: "mongodb+srv://oluwatoyinjolaoso001:Jobs123.@cluster0.lyzft9g.mongodb.net/",
+  react_app_url: "http://localhost:4200"
+};
